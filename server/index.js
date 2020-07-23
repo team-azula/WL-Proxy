848d80c1237a145d24596c69b394c33908191c79
EC2 Default User <ec2-user@ip-172-31-21-207.us-east-2.compute.internal>
2020-07-23 03:53:18 +0000

    Commented out things that didn't help me deploy

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const path = require('path');
+//const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
 
-//app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '/../client'));
 
-app.use(express.static(path.join(__dirname, '../client')));
+//app.use(express.static(path.join(__dirname, '../client')));
 
 
 //app.get('/:id', (req, res) => {
@@ -15,11 +15,11 @@ app.use(express.static(path.join(__dirname, '../client')));
 //});
 
 
-app.get('./bundle.js/:3004', (req, res) => {
-   request('http://ec2-3-23-61-11.us-east-2.compute.amazonaws.com:3004/bundle.js', (err, response, body) => {
-	res.status(200).send(body);
-   })
- })
+//app.get('./bundle.js/:3004', (req, res) => {
+//   request('http://ec2-3-23-61-11.us-east-2.compute.amazonaws.com:3004/bundle.js', (err, response, body) => {
+//	res.status(200).send(body);
+//   })
+// })
 
 
 app.listen(PORT, () => {
