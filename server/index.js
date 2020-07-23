e3a3da34444686d4a9e64f97ef6370ffad968ac1
EC2 Default User <ec2-user@ip-172-31-21-207.us-east-2.compute.internal>
2020-07-23 02:17:21 +0000

    Proxy Deployed

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname + '/../client'));
+
+//app.use(express.static(__dirname + '/../client'));
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+
+//app.get('/:id', (req, res) => {
+//  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+//});
+
+
+app.get('./bundle.js/:3004', (req, res) => {
+   request('http://ec2-3-23-61-11.us-east-2.compute.amazonaws.com:3004/bundle.js', (err, response, body) => {
+	res.status(200).send(body);
+   })
+ })
+
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
